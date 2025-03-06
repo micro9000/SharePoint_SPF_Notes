53fd0ed80d2f38ed462dfa0c6aa6fb1b749a8f6c
Raniel Garcia <ranielgarcia2596@gmail.com>
2025-03-06 21:54:22 +0800

    CRUD LIst using PnP JS

---
+++ SharePointSPFxProjects/CRUDSharePointList/src/webparts/crudSharepointPnpjs/components/ICrudSharepointPnpjsProps.ts
@@ -0,0 +1,7 @@
+export interface ICrudSharepointPnpjsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ SharePointSPFxProjects/CRUDSharePointList/src/webparts/crudSharepointPnpjs/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICrudSharepointPnpjsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CrudSharepointPnpjsWebPartStrings' {
+  const strings: ICrudSharepointPnpjsWebPartStrings;
+  export = strings;
+}
