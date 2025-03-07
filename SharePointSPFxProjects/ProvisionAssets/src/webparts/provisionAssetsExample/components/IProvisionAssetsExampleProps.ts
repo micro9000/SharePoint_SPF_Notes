09977566fc2ec56ee85d81f5f02310cb4277b090
Raniel Garcia <ranielgarcia2596@gmail.com>
2025-03-07 09:52:48 +0800

    Moved all notes into folder to utilize the Obsidian note taking app

---
+++ SharePointSPFxProjects/ProvisionAssets/src/webparts/provisionAssetsExample/components/IProvisionAssetsExampleProps.ts
@@ -0,0 +1,7 @@
+export interface IProvisionAssetsExampleProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ SharePointSPFxProjects/ProvisionAssets/src/webparts/provisionAssetsExample/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IProvisionAssetsExampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ProvisionAssetsExampleWebPartStrings' {
+  const strings: IProvisionAssetsExampleWebPartStrings;
+  export = strings;
+}
